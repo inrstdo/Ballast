97f884e70be213919e0ec80b366d5095f5bccbe8
Eller-Romey, Ian <ian.ellerromey@nike.com>
2020-06-30 08:12:17 -0700

    Committing code that won't work because this laptop is going away

--- MemoryManager_Lib/headers/heterogeneousAllocator.h
+++ MemoryManager_Lib/headers/heterogeneousAllocator.h
@@ -8,33 +8,107 @@ namespace Ballast {
 
     class HeterogeneousAllocator
     {
-      public:
-        HeterogeneousAllocator(const unsigned size);
-        ~HeterogeneousAllocator();
-
-
       private:
-        class Node
+        class Page
         {
-          public:
-            Node(const unsigned size, Node* const left, Node* const right );
-
-            void setLeft(Node* left);
-            void setRight(Node* right);
-
-            Node* const getLeft() const;
-            Node* const getRight() const;
-
-
           private:
-            const unsigned c_size;
-            Node* m_left;
-            Node* m_right;
+            struct Block
+            {
+              Block(const unsigned size, const bool free, Block* const left, Block* const right, Block* parent);
+              ~Block();
+
+
+              static Block* const allocateNew(char* const freePoolLocation, const unsigned blockSize, const bool free, Block* const prev, Block* const next);
+
+
+              unsigned m_size;
+              bool m_free;
+              Block* m_prev;
+              Block* m_next;
+            };
+
+
+            Block* const traverseListAlloateNewBlock(unsigned const size, Block* const parentBlock);
+
+            
+            const unsigned c_totalSize;
+            unsigned m_availableSize;
+            char* m_freePool;
+            Block* m_parentNode;
+            Page* m_next;
+
+
+          public:
+            Page(const unsigned pageSize, char* const freePool);
+            ~Page();
+
+            const unsigned getAvailableSize() const;
+            Page* const getNext() const;
+
+            void setNext(Page* const nextPage);
+
+            template<typename T>
+            T* allocate(const unsigned trueSize)
+            {
+              Block* currentNode = m_parentNode;
+
+              return 0;
+            }
+
+            template<typename T>
+            void deallocate(T* memory, unsigned size)
+            {
+
+            }
+
+
+            static Page* const allocateNew(const unsigned pageSize);
         };
 
 
-        const unsigned c_size;
-        char* m_freePool;
+        Page* const getLastPage() const;
+
+
+        const unsigned c_pageSize;
+        Page* const c_pageList;
+
+
+      public:
+        HeterogeneousAllocator(const unsigned pageSize);
+        ~HeterogeneousAllocator();
+
+
+        template<typename T>
+        T* allocate(unsigned size)
+        {
+          const trueSize = size * sizeof(T);
+
+          if (trueSize > c_pageSize)
+          {
+            throw "Cannot allocate any object larger than c_pageSize";
+          }
+
+          Page* currentPage = c_pageList;
+
+          while(currentPage->getAvailableSize() < trueSize)
+          {
+            if(!currentPage->getNext())
+            {
+              Page* nextPage = Page::allocateNew(c_pageSize);
+              currentPage->setNext(nextPage);
+            }
+
+            currentPage = currentPage->getNext();
+          }
+
+          return currentPage->allocate<T>(size);
+        }
+
+        template<typename T>
+        void deallocate(T* memory, unsigned size)
+        {
+
+        }
     };
     
 
--- MemoryManager_Lib/source/heterogenousAllocator.cpp
+++ MemoryManager_Lib/source/heterogenousAllocator.cpp
@@ -1,3 +1,4 @@
+#include <new>
 #include "../headers/heterogeneousAllocator.h"
 
 
@@ -5,48 +6,136 @@ namespace Ballast {
   namespace MemoryManager {
 
 
-    HeterogeneousAllocator::HeterogeneousAllocator(const unsigned size) :
-      c_size(size)
+    HeterogeneousAllocator::HeterogeneousAllocator(const unsigned pageSize) :
+      c_pageSize(pageSize),
+      c_pageList(Page::allocateNew(c_pageSize))
     {
-      m_freePool = new char[c_size];
     }
 
 
     HeterogeneousAllocator::~HeterogeneousAllocator()
     {
-      delete [] m_freePool;
+      Page* currentPage = c_pageList;
+
+      while(currentPage)
+      {
+        char* memory = reinterpret_cast<char*>(currentPage);
+
+        currentPage = currentPage->getNext();
+        
+        delete [] memory;
+      }
     }
+
+
+    HeterogeneousAllocator::Page* const HeterogeneousAllocator::getLastPage() const
+    {
+      Page* currentPage = c_pageList;
+
+      while(currentPage->getNext())
+      {
+        currentPage = currentPage->getNext();
+      }
+
+      return currentPage;
+    }
+
+
+    HeterogeneousAllocator::Page::Page(const unsigned pageSize, char* const freePool) :
+      c_totalSize(pageSize),
+      m_availableSize(c_totalSize),
+      m_freePool(freePool),
+      m_parentNode(Block::allocateNew(m_freePool + sizeof(Page), pageSize, true, 0, 0, 0))
+    {
+    }
+
+
+    HeterogeneousAllocator::Page* const HeterogeneousAllocator::Page::allocateNew(const unsigned pageSize)
+    {
+      const unsigned trueSize = pageSize + sizeof(Page) + sizeof(Block);
+      const char* memory = new char[trueSize];
+      ::new reinterpret_cast<Page*>(memory) Page(pageSize, memory + sizeof(Page));
+
+      return reinterpret_cast<Page*>(memory);
+    }
+
+
+    const unsigned HeterogeneousAllocator::Page::getAvailableSize() const
+    {
+      return m_availableSize;
+    }
+
+
+    HeterogeneousAllocator::Page* const HeterogeneousAllocator::Page::getNext() const
+    {
+      return m_next;
+    }
+
+
+    void HeterogeneousAllocator::Page::setNext(Page* const nextPage)
+    {
+      m_next = nextPage;
+    }
+
     
-    
-    HeterogeneousAllocator::Node::Node(const unsigned size, Node* const left, Node* const right) :
-      c_size(size),
+    HetereogeneousAllocator::Page::Block* const HeterogeneousAllocator::Page::traverseTreeCreateNewBlock(unsigned const size, Block* const parentBlock)
+    {
+      // if the current Block has capacity and is free
+      if(parentBlock->m_size >= size && parentBlock->m_free)
+      {
+        Block* newLeftBlock = 0;
+
+        // if there's capacity for another Block after this one
+        if(parentBlock->m_size - size > sizeof(Block))
+        {
+          char* const memory = reinterpret_cast<const char*>(parentBlock);
+          newLeftBlock = Block::allocateNew(memory + size, parentBlock->m_size - size - sizeof(Block), true, m_parentNode->m_left, 0, m_parentNode);
+        }
+
+        parentBlock->m_size = size;
+        parentBlock->m_size = false;
+
+        if(newLeftBlock)
+        {
+          parentBlock->m_left = newLeftBlock;
+        }
+
+        return parentBlock;
+      }
+
+      if(parentBlock->m_left)
+      {
+        Block* allocatedBlock = traverseTreeCreateNewBlock(size, parentBlock->m_left);
+
+        if (allocatedBlock)
+        {
+          return allocatedBlock;
+        }
+      }
+
+      const char* freePoolLocation = reinterpret_cast<const char*>(parentBlock);
+      freePoolLocation += sizeof(Block) + parentBlock->c_size;
+
+      Block* newBlock = Block::allocateNew(size, 
+    }
+
+
+    HeterogeneousAllocator::Page::Block* const HeterogeneousAllocator::Page::Block::allocateNew(char* const freePoolLocation, const unsigned blockSize, const bool free, Block* const left, Block* const right, Block* parent)
+    {
+      ::new reinterpret_cast<Block*>(freePoolLocation) Block(blockSize, free, left, right, parent);
+
+      return reinterpret_cast<Block*>(freePoolLocation);
+    }
+
+
+    HeterogeneousAllocator::Page::Block::Block(const unsigned size, const bool free, Block* const left, Block* const right, Block* const parent) :
+      m_size(size),
+      m_free(free),
       m_left(left),
-      m_right(right)
+      m_right(right),
+      m_parent(parent)
     {
-    }
 
-
-    void HeterogeneousAllocator::Node::setLeft(Node* const left)
-    {
-      m_left = left;
-    }
-
-
-    void HeterogeneousAllocator::Node::setRight(Node* const right)
-    {
-      m_right = right;
-    }
-
-
-    HeterogeneousAllocator::Node* const HeterogeneousAllocator::Node::getLeft() const
-    {
-      return m_left;
-    }
-
-
-    HeterogeneousAllocator::Node* const HeterogeneousAllocator::Node::getRight() const
-    {
-      return m_right;
     }
 
 
